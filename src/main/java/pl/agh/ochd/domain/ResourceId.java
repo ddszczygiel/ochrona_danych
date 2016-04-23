0e56cf796eba94d3127746f187390e303064b65f
mciolek <maciek.ciolek@gmail.com>
2016-04-23 23:23:18 +0200

    API for persistence introduced.

---
+++ src/main/java/pl/agh/ochd/domain/ResourceId.java
@@ -0,0 +1,21 @@
+package pl.agh.ochd.domain;
+
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * Repersents the resource (server, vm, application etc.) which is under monitoring.
+ */
+public class ResourceId {
+    private final String value;
+
+    public ResourceId(String value) {
+        if (StringUtils.isEmpty(value)) {
+            throw new IllegalArgumentException("Value cannot be null or empty.");
+        }
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
---
+++ src/main/java/pl/agh/ochd/persistence/PersistenceService.java
@@ -0,0 +1,36 @@
+package pl.agh.ochd.persistence;
+
+
+import pl.agh.ochd.domain.LogSample;
+import pl.agh.ochd.domain.ResourceId;
+
+import java.util.Collection;
+import java.util.Date;
+import java.util.Optional;
+import java.util.regex.Pattern;
+
+/**
+ * Implementation of this is responsible of storing and loading logs.
+ */
+public interface PersistenceService {
+
+    /**
+     * Use in order to store new logs.
+     *
+     * @param id   monitored resource id
+     * @param logs collection of log samples to store
+     */
+    void saveLogs(ResourceId id, Collection<LogSample> logs);
+
+    /**
+     * Use in order to load logs within specified reange of time. If there is no pattern, all samples will be returned.
+     *
+     * @param id      monitored resource id
+     * @param from    the begin of time range
+     * @param to      the end of time range
+     * @param pattern optional pattern to match
+     * @return returns collection of samples
+     */
+    Collection<LogSample> loadLogs(ResourceId id, Date from, Date to, Optional<Pattern> pattern);
+
+}
